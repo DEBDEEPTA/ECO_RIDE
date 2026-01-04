82f735c12e433c610a51be708e31104369ea2b3b
DEBDEEPTA <deep.o8.2oo3.26@gmail.com>
2026-01-04 19:23:03 +0530

    initial project structure

---
+++ src/EcoRideMain.py
@@ -0,0 +1,14 @@
+class EcoRideMain:
+    def greet(self):
+        print("Welcome to Eco-Ride Urban Mobility System")
+
+
+
+
+def main():
+    ec_ride = EcoRideMain()
+    ec_ride.greet()
+
+
+if __name__=="__main__":
+    main()
